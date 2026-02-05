b35e2ab90b76e3c603ad0770dad2444af7d9a51b
Francesca-Magliulo <github.mf@gmail.com>
2026-02-05 14:13:55 +0100

    ex02 completed

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -15,7 +15,16 @@ console.log(reversedTeachers)
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = [];
+
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i].length >= 5) {
+
+    longNames.push(teachers[i])
+  }
+}
+
+console.log(longNames)
 
 // 3. Rimuovi 'Ed' dall'array teachers
 teachers.splice(5, 1)
@@ -23,7 +32,15 @@ console.log(teachers)
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+let fabioIndex = teachers.indexOf("Fabio")
+console.log(fabioIndex)
 
-// 5. Unisci tutti gli insegnanti nell'array teachers in una stringa separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const isFabioPresent = fabioIndex !== -1;
+
+console.log(isFabioPresent)
+
+// 5. Unisci tutti gli insegnanti nell'array teachers in una stringa
+//  separata da virgole e salvala nella variabile teachersString
+const teachersString = teachers.toString();
+
+console.log(teachersString)
